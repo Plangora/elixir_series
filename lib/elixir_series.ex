0c2d679a9fbe035c9f47c3fc27b1a9a9087e6ecb
Allen Wyma <allen.wyma@gmail.com>
2019-02-15 17:46:19 +0800

    converted into an elixir application

---
+++ lib/elixir_series.ex
@@ -0,0 +1,18 @@
+defmodule ElixirSeries do
+  @moduledoc """
+  Documentation for ElixirSeries.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirSeries.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_series_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirSeriesTest do
+  use ExUnit.Case
+  doctest ElixirSeries
+
+  test "greets the world" do
+    assert ElixirSeries.hello() == :world
+  end
+end
